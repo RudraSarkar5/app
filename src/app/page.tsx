af229155f774a50613da138c605bd4c9ef408c0f
RudraSarkar5 <rudrasarkar311@gmail.com>
2024-11-09 09:14:48 +0530

    changed happedfdfn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@ import { Metadata } from "next";
 export  const metadata:Metadata = {
    title : "Home"
 }
+
 export default function Home() {
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {process.env.HOST}
+      <h1>it is me rudra also what is goging </h1>{process.env.HOST}
     </div>
   );
 }
